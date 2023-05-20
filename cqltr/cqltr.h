60dc1661d4ce5b76cb080f4c6c4fa2fc6fab881a
cnjawi <cnjawi@yeah.net>
2023-05-20 12:43:36 +0800

    移除__declspec属性

--- cqltr/cqltr.h
+++ cqltr/cqltr.h
@@ -2,11 +2,5 @@
 
 #include <string>
 
-#ifndef CQLTR_EXPORTS
-#define CQLTR_API __declspec(dllexport)
-#else
-#define CQLTR_API __declspec(dllimport)
-#endif
-
-extern "C" CQLTR_API const char* figure(int[4]);
-extern "C" CQLTR_API bool validate(int[4]);
+extern "C" const char* figure(int[4]);
+extern "C" bool validate(int[4]);
